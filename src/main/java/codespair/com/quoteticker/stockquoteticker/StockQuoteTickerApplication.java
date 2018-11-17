4b5bfd4d26d8c5afbb82041efb3666782f0b9c7e
Marcos Maia <maia.marcos@gmail.com>
2018-11-17 11:42:26 +0100

    initial project

---
+++ src/main/java/codespair/com/quoteticker/stockquoteticker/StockQuoteTickerApplication.java
@@ -0,0 +1,12 @@
+package codespair.com.quoteticker.stockquoteticker;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StockQuoteTickerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StockQuoteTickerApplication.class, args);
+	}
+}
